24003af6aa28a370b4ab21b9d88ba4edd6730c63
Shane McDonough <shanemcdman@gmail.com>
2020-07-24 16:09:48 -0700

    Allow rotate_point to adjust for center

--- Clock.py
+++ Clock.py
@@ -24,14 +24,14 @@ class Clock(Game):
             theta = (i * 30) / 180 * np.pi
             offset = (6 if i % 3 == 0 else 4)
             width = (2 if i % 3 == 0 else 1)
-            p1 = 0, self.radius + offset
-            p2 = 0, self.radius - offset
+            p1 = self.center[0], self.radius + offset + self.center[1]
+            p2 = self.center[0], self.radius - offset + self.center[1]
             p1 = self.rotate_point(p1, theta, self.center)
             p2 = self.rotate_point(p2, theta, self.center)
             pygame.draw.line(self.screen, (255, 255, 255), p1, p2, width)
 
     @staticmethod
     def rotate_point(point: ('x', 'y'), theta: "radians", center: ('x', 'y')) -> ('x', 'y'):
-        x = point[0] * np.cos(theta) + point[1] * np.sin(theta) + center[0]
-        y = point[0] * -np.sin(theta) + point[1] * np.cos(theta) + center[1]
+        x = (point[0] - center[0]) * np.cos(theta) + (point[1] - center[1]) * np.sin(theta) + center[0]
+        y = (point[0] - center[0]) * -np.sin(theta) + (point[1] - center[1]) * np.cos(theta) + center[1]
         return(x, y)
